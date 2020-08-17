020ff23d3d1f6d8e18c9800a99b30b183bce7688
Nhu Quynh <ngngocnhuquynh0104@gmail.com>
2020-08-17 22:09:51 +0700

    UI/Add transaction

---
+++ src/components/GroupItem.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./GroupItem.css";
+
+function GroupItem({ group, onClick }) {
+  return (
+    <div className="GroupItem" onClick={onClick}>
+      <img src={group.icon} alt="icon" />
+      <p>{group.name}</p>
+    </div>
+  );
+}
+
+export default GroupItem;
--- src/pages/AddTransaction.js
+++ src/pages/AddTransaction.js
@@ -1,10 +1,38 @@
-import React from "react";
+import React, { useRef } from "react";
 import NavigateNextIcon from "@material-ui/icons/NavigateNext";
 import SubjectIcon from "@material-ui/icons/Subject";
 import EventIcon from "@material-ui/icons/Event";
+import Modal from "@material-ui/core/Modal";
+import { GroupContext } from "../contexts/Group";
+import GroupItem from "../components/GroupItem";
 import "./AddTransaction.css";
 
 function AddTransaction() {
+  const [open, setOpen] = React.useState(false);
+  const [type, setType] = React.useState(0);
+  const [amountValue, setAmountValue] = React.useState(0);
+  const nameGroup = useRef(null);
+  const iconGroup = useRef(null);
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const handleChange = (event) => {
+    setAmountValue(event.target.value);
+  }
+  console.log(amountValue);
+
+  function selectGroup(name, icon) {
+    nameGroup.current.innerHTML = name;
+    iconGroup.current.setAttribute('style', 'background-image: url('+ icon + ')');
+    setOpen(false);
+  }
+
   function getToday() {
     const today = new Date();
     return (
@@ -26,16 +54,53 @@ function AddTransaction() {
           <div className="icon vnd">VND</div>
           <div className="content amount">
             <p>Số tiền</p>
-            <input type="text" defaultValue={0} />
+            <input type="text" defaultValue={0} value={Number(amountValue)} onChange={handleChange}/>
           </div>
         </div>
-        <div className="item group">
-          <div className="icon"></div>
+        <div className="item group" onClick={handleOpen}>
+          <div ref={iconGroup} className="icon"></div>
           <div className="content">
-            <span>Chọn nhóm</span>
+            <span ref={nameGroup}>Chọn nhóm</span>
             <NavigateNextIcon className="moreIcon" />
           </div>
         </div>
+        <Modal open={open} onClose={handleClose}>
+          <div className="Modal">
+            <div className="headerModal">
+              <button
+                className={type === 2 ? "active" : null}
+                onClick={() => setType(2)}
+              >
+                Đi vay/Cho vay
+              </button>
+              <button
+                className={type === 0 ? "active" : null}
+                onClick={() => setType(0)}
+              >
+                Khoản chi
+              </button>
+              <button
+                className={type === 1 ? "active" : null}
+                onClick={() => setType(1)}
+              >
+                Khoản thu
+              </button>
+            </div>
+            <div className="mainModal">
+              <GroupContext.Consumer>
+                {({ groups }) => {
+                  return groups.map((group, index) => {
+                    return (
+                      group.type === type && (
+                        <GroupItem group={group} key={index} onClick={() => selectGroup(group.name, group.icon)}/>
+                      )
+                    );
+                  });
+                }}
+              </GroupContext.Consumer>
+            </div>
+          </div>
+        </Modal>
         <div className="item">
           <div className="icon">
             <SubjectIcon className="imgIcon" />
